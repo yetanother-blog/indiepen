ca0884138c2929c711ac50222a3d40f0866e5663
Henrik Fricke <henrikfricke@web.de>
2021-06-03 18:40:40 +0200

    feat: configure prettier

--- snowpack.config.js
+++ snowpack.config.js
@@ -2,21 +2,19 @@
 // See all supported options: https://www.snowpack.dev/reference/configuration
 module.exports = {
   mount: {
-    src: { 
-      url: '/', 
-      static: false, 
-      resolve: true 
+    src: {
+      url: '/',
+      static: false,
+      resolve: true,
     },
-    public: { 
-      url: '/', 
-      static: true, 
-      resolve: false 
-    }
+    public: {
+      url: '/',
+      static: true,
+      resolve: false,
+    },
   },
   devOptions: {
-    openUrl: '?url=http%3A%2F%2Flocalhost%3A8080%2Fexample'
+    openUrl: '?url=http%3A%2F%2Flocalhost%3A8080%2Fexample',
   },
-  exclude: [
-    'public/**/*'
-  ]
+  exclude: ['public/**/*'],
 };
