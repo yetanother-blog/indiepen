7964dc669ee0641e0bbfea2f0c13e10d6259e1a1
Henrik Fricke <henrikfricke@web.de>
2021-06-06 19:19:37 +0200

    feat(integration): tests for startpage

---
+++ cypress/integration/startpage_spec.ts
@@ -0,0 +1,24 @@
+describe('Indiepen Startpage', () => {
+  it('Opens the startpage', () => {
+    cy.visit('/');
+  });
+
+  it('Shows a title', () => {
+    cy.get('h1').contains('Embed your HTML, CSS and JS example with Indiepen');
+  });
+
+  it('Generates, copies and resets code snippet', () => {
+    cy.contains('Your code example URL (required)')
+      .click()
+      .type('https://example.com');
+    cy.contains('Generate').click();
+    cy.contains('Copy code').click();
+    cy.task('getClipboard').should(
+      'contain',
+      '<iframe class="indiepen" src="https://indiepen.tech/?url=https%3A%2F%2Fexample.com" style="width: 100%; overflow: hidden; display: block" title="Indiepen Embed" loading="lazy" width="100%" height="450" frameborder="0"></iframe>'
+    );
+    cy.contains('Reset').click();
+    cy.get('#generated-result').should('be.disabled', true);
+    cy.contains('Reset').should('be.disabled', true);
+  });
+});
