0409cc12b9400bbfa81406bf3e14952f194e709a
Henrik Fricke <henrikfricke@web.de>
2021-06-06 18:22:21 +0200

    fix(integration): update path for embed spec

--- cypress/integration/embed_spec.ts
+++ cypress/integration/embed_spec.ts
@@ -1,6 +1,6 @@
 describe('Indiepen Embed', () => {
   it('Opens the embed tool', () => {
-    cy.visit('?url=http%3A%2F%2Flocalhost%3A8080%2Fexample');
+    cy.visit('embed?url=http%3A%2F%2Flocalhost%3A8080%2Fexample');
   });
 
   it('Shows the example project', () => {
