6974972475b661291eda335c7eda5e33b301438d
Henrik Fricke <henrikfricke@web.de>
2021-06-10 23:01:45 +0200

    tests(embed): click around to test the navigation

--- cypress/integration/embed_spec.ts
+++ cypress/integration/embed_spec.ts
@@ -1,9 +1,64 @@
 describe('Indiepen Embed', () => {
-  it('Opens the embed tool', () => {
-    cy.visit('embed?url=http%3A%2F%2Flocalhost%3A8080%2Fexample');
+  describe('desktop', () => {
+    it('Opens the embed tool', () => {
+      cy.visit('embed?url=/example');
+    });
+
+    it('Shows the example project', () => {
+      cy.get('.preview.result').its('0.contentDocument').contains('👋');
+    });
+
+    it('Previews html by default', () => {
+      cy.contains('<h1>👋</h1>');
+    });
+
+    it('Previews css after clicking', () => {
+      cy.contains('CSS').click();
+      cy.contains('body {');
+    });
+
+    it('Previews html after clicking', () => {
+      cy.contains('HTML').click();
+      cy.contains('<h1>👋</h1>');
+    });
+
+    it('Previews js after clicking', () => {
+      cy.contains('JS').click();
+      cy.contains(`console.log('Hey 👋');`);
+    });
   });
 
-  it('Shows the example project', () => {
-    cy.contains('👋');
+  describe('mobile', () => {
+    beforeEach(() => {
+      cy.viewport(375, 812);
+    });
+
+    it('Opens the embed tool', () => {
+      cy.visit('embed?url=/example');
+    });
+
+    it('Shows html by default', () => {
+      cy.contains('<h1>👋</h1>');
+    });
+
+    it('Shows the example project after clicking', () => {
+      cy.contains('Result').click();
+      cy.get('.preview.result').its('0.contentDocument').contains('👋');
+    });
+
+    it('Previews html after clicking', () => {
+      cy.contains('HTML').click();
+      cy.contains('<h1>👋</h1>');
+    });
+
+    it('Previews css after clicking', () => {
+      cy.contains('CSS').click();
+      cy.contains('body {');
+    });
+
+    it('Previews js after clicking', () => {
+      cy.contains('JS').click();
+      cy.contains(`console.log('Hey 👋');`);
+    });
   });
 });
