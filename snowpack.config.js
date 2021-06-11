cf95ca7c0e4bcaea69a7afd0d12945b8b7290c40
Henrik Fricke <henrikfricke@web.de>
2021-06-11 10:02:55 +0200

    feat(snowpack): enable bundle optimization

--- snowpack.config.js
+++ snowpack.config.js
@@ -18,5 +18,10 @@ module.exports = {
       resolve: true,
     },
   },
+  optimize: {
+    bundle: true,
+    minify: true,
+    target: 'es2018',
+  },
   exclude: ['public/**/*', '**/LICENSE'],
 };
